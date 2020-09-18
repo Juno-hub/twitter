02c2d72094d7fda1a2db5df4e87dcd555be09381
Juno <bae3054@naver.com>
2020-09-18 16:34:12 +0900

    #1.1 Securing the Keys at only Github

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5JQdVViLsXL1cPofeEMH68PzQpikmp2Y",
-  authDomain: "twitter-clone-f9bbc.firebaseapp.com",
-  databaseURL: "https://twitter-clone-f9bbc.firebaseio.com",
-  projectId: "twitter-clone-f9bbc",
-  storageBucket: "twitter-clone-f9bbc.appspot.com",
-  messagingSenderId: "1033290088840",
-  appId: "1:1033290088840:web:dc59869baa1f884481ba3d",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
