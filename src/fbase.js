bd6c9396ec6b7fc2b250b0b1f52d01ef7aa6842e
Juno <bae3054@naver.com>
2020-09-19 17:57:47 +0900

    #2.0 Using Firebase Auth

---
+++ src/fbase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5JQdVViLsXL1cPofeEMH68PzQpikmp2Y",
+  authDomain: "twitter-clone-f9bbc.firebaseapp.com",
+  databaseURL: "https://twitter-clone-f9bbc.firebaseio.com",
+  projectId: "twitter-clone-f9bbc",
+  storageBucket: "twitter-clone-f9bbc.appspot.com",
+  messagingSenderId: "1033290088840",
+  appId: "1:1033290088840:web:dc59869baa1f884481ba3d",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
