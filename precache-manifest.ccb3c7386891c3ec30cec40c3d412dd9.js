bb386d0dd0660784c8d0d2227a13ce6ecf7b54c8
Juno <bae3054@naver.com>
2020-10-12 17:21:02 +0900

    Updates

---
+++ precache-manifest.ccb3c7386891c3ec30cec40c3d412dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7894182e7655e458095beb6bc6f55804",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "1a6519bcdb73730752d0",
+    "url": "/twitter/static/css/main.9bc650d6.chunk.css"
+  },
+  {
+    "revision": "f9776c5d06d167aa6cd0",
+    "url": "/twitter/static/js/2.b3986b2a.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/twitter/static/js/2.b3986b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6519bcdb73730752d0",
+    "url": "/twitter/static/js/main.4a6a1211.chunk.js"
+  },
+  {
+    "revision": "9f20f10883ff1630816b",
+    "url": "/twitter/static/js/runtime-main.39cb9049.js"
+  }
+]);
